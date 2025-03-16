f100c30651a870f0dfeb0bb53785fb2735e3216d
akiaki0219 <akiaki.he.na@gmail.com>
2025-03-16 23:14:44 +0900

    Analyticモデルを追加

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Analytic(models.Model):
+  videoId = models.IntegerField()
+  videoTitle = models.CharField(max_length=100)
+  get_at = models.DateField(default=timezone.now)
+  YouTubeView = models.PositiveIntegerField()
+  niconicoView = models.PositiveIntegerField()
+  YouTubeLike = models.PositiveIntegerField()
+  niconicoLike = models.PositiveIntegerField()
+  YouTubeComment = models.PositiveIntegerField()
+  niconicoComment = models.PositiveIntegerField()
+  niconicoMylist = models.PositiveIntegerField()
