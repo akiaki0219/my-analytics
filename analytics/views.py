eca576f61a423bfba3ef210801fe6d18e63a51fe
akiaki0219 <akiaki.he.na@gmail.com>
2025-03-17 00:40:54 +0900

    トップページを追加

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,24 @@
+from analytics.models import Analytic
+from datetime import date
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  if request.method == "POST":
+    analytic = Analytic(
+      videoId=request.POST['videoId'],
+      videoTitle=request.POST['videoTitle'],
+      get_at=date.today(),
+      YouTubeView=request.POST['YouTubeView'],
+      YouTubeLike=request.POST['YouTubeLike'],
+      YouTubeComment=request.POST['YouTubeComment'],
+      niconicoView=request.POST['niconicoView'],
+      niconicoLike=request.POST['niconicoLike'],
+      niconicoComment=request.POST['niconicoComment'],
+      niconicoMylist=request.POST['niconicoMylist']
+    )
+    analytic.save()
+  analytics = Analytic.objects.all()
+  context = {
+    'analytics': analytics
+  }
+  return render(request, "analytics/index.html", context)
